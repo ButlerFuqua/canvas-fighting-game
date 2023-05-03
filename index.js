7bc4c182099b055ff76b7411d48f410ec7ce89cd
butlerfuqua <butlerfuqua@gmail.com>
2023-05-02 19:33:05 -1000

    Refactor into classes file

--- index.js
+++ index.js
@@ -9,64 +9,7 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = .7;
 
-class Sprite {
-    constructor({ position, velocity, color = 'red', offset }) {
-        this.position = position
-        this.velocity = velocity
-        this.height = 150
-        this.width = 50
-        this.lastKey
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset,
-            width: 100,
-            height: 50,
-        }
-        this.color = color
-        this.isAttacking = false
-        this.health = 100
-    }
-
-    draw() {
-        const { x: spriteX, y: spriteY } = this.position
-        c.fillStyle = this.color
-        c.fillRect(spriteX, spriteY, this.width, this.height)
-
-        // attackBox
-        if (this.isAttacking) {
-            const { x: attackX, y: attackY } = this.attackBox.position;
-            const { width: attackW, height: attackH } = this.attackBox
-            c.fillStyle = 'green'
-            c.fillRect(attackX, attackY, attackW, attackH)
-        }
-    }
-
-    update() {
-        this.draw()
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y + this.attackBox.offset.y
-
-
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-
-        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-            this.velocity.y = 0;
-        } else {
-            this.velocity.y += gravity;
-        }
-    }
-
-    attack() {
-        this.isAttacking = true;
-        setTimeout(() => this.isAttacking = false, 100)
-    }
-}
-
-const player = new Sprite({
+const player = new Fighter({
     position: {
         x: 0,
         y: 0,
@@ -81,7 +24,7 @@ const player = new Sprite({
     }
 })
 
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {
         x: 400,
         y: 100
---
+++ js/classes.js
@@ -0,0 +1,71 @@
+class Sprite {
+    constructor({ position, }) {
+        this.position = position
+        this.height = 150
+        this.width = 50
+    }
+
+    draw() {
+
+    }
+
+    update() {
+        this.draw()
+    }
+}
+class Fighter {
+    constructor({ position, velocity, color = 'red', offset }) {
+        this.position = position
+        this.velocity = velocity
+        this.height = 150
+        this.width = 50
+        this.lastKey
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset,
+            width: 100,
+            height: 50,
+        }
+        this.color = color
+        this.isAttacking = false
+        this.health = 100
+    }
+
+    draw() {
+        const { x: spriteX, y: spriteY } = this.position
+        c.fillStyle = this.color
+        c.fillRect(spriteX, spriteY, this.width, this.height)
+
+        // attackBox
+        if (this.isAttacking) {
+            const { x: attackX, y: attackY } = this.attackBox.position;
+            const { width: attackW, height: attackH } = this.attackBox
+            c.fillStyle = 'green'
+            c.fillRect(attackX, attackY, attackW, attackH)
+        }
+    }
+
+    update() {
+        this.draw()
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y = this.position.y + this.attackBox.offset.y
+
+
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0;
+        } else {
+            this.velocity.y += gravity;
+        }
+    }
+
+    attack() {
+        this.isAttacking = true;
+        setTimeout(() => this.isAttacking = false, 100)
+    }
+}
