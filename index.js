ea95177623f3ede35df89d46632231c3e62a20fc
butlerfuqua <butlerfuqua@gmail.com>
2023-05-02 19:37:51 -1000

    Render image for background

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = .7;
 
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    imgSrc: './assets/background.png'
+})
 const player = new Fighter({
     position: {
         x: 0,
@@ -107,6 +114,7 @@ function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
+    background.update()
     player.update()
     enemy.update()
 
